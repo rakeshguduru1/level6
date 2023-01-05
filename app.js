197f30d82112f58535a181f0e865c1f0fb097263
rakeshguduru1 <104123905+rakeshguduru1@users.noreply.github.com>
2023-01-05 22:52:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ app.get("/", function (request, response) {
 app.get("/todos", async function (_request, response) {
   console.log("Processing list of all Todos ...");
   // FILL IN YOUR CODE HERE
+  try {
+    const todos = await Todo.findAll();
+    return response.send(todos);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
 
   // First, we have to query our PostgerSQL database using Sequelize to get list of all Todos.
   // Then, we have to respond with all Todos, like:
@@ -51,7 +58,8 @@ app.put("/todos/:id/markAsCompleted", async function (request, response) {
 app.delete("/todos/:id", async function (request, response) {
   console.log("We have to delete a Todo with ID: ", request.params.id);
   // FILL IN YOUR CODE HERE
-
+  const deleteTodo = await Todo.destroy({ where: { id: request.params.id } });
+  response.send(deleteTodo ? true : false);
   // First, we have to query our database to delete a Todo by ID.
   // Then, we have to respond back with true/false based on whether the Todo was deleted or not.
   // response.send(true)

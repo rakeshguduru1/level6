55af86a49bb3f39f72c7760af80802f9bca2418f
rakeshguduru1 <104123905+rakeshguduru1@users.noreply.github.com>
2023-01-05 22:42:53 +0530

    Update todos.js

--- __tests__/todos.js
+++ __tests__/todos.js
@@ -73,5 +73,16 @@ describe("Todo Application", function () {
 
   test("Deletes a todo with the given ID if it exists and sends a boolean response", async () => {
     // FILL IN YOUR CODE HERE
+    const response = await agent.post("/todos").send({
+      title: "Buy Everything",
+      dueDate: new Date().toISOString(),
+      completed: false,
+    });
+    const parsedResponse = JSON.parse(response.text);
+    const todoID = parsedResponse.id;
+
+    const deleteTodoResponse = await agent.delete(`/todos/${todoID}`).send();
+    const parsedDeleteResponse = JSON.parse(deleteTodoResponse.text);
+    expect(parsedDeleteResponse).toBe(true);
   });
 });
